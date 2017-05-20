ca9cc5a3f54fb45c09cef9801ba8661c942e74b7
IgorK <ikuplevich@fitbit.com>
2017-05-20 10:01:32 -0700

    interim commit

--- hr/src/Solution.java
+++ hr/src/Solution.java
@@ -1,37 +1,151 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Solution {
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner in = new Scanner(new FileInputStream("hr/src/in"));
-        int n = in.nextInt(), s = in.nextInt(), t = in.nextInt();
-        int[] r = new int[n];
-        r[0] = in.nextInt();
-        int g = in.nextInt(), seed = in.nextInt(), p = in.nextInt();
-        if (s == t) System.out.println(0);
-        else {
-            for (int i = 1; i < n; i++)
-                r[i] = (r[i - 1] * g + seed) % p;
-            int seconds = seconds(s, t, r, n);
-            System.out.println(seconds);
+        Scanner in = new Scanner(System.in);
+        //Scanner in = new Scanner(new FileInputStream("hr/src/in"));
+        OrderBook book = new OrderBook();
+        int id = 1;
+        while (in.hasNext()) {
+            String str = in.nextLine();
+            String[] s = str.split(" ");
+            Order order = new Order(id++, s[0], s[1], Integer.parseInt(s[2]), Double.parseDouble(s[3]));
+            book.process(order);
         }
-
     }
 
-    static int seconds(int s, int t, int[] r, int n) {
-        if (s == t) return 0;
-        if (r[s] == 0) return -1;
-        int min = 1;
-        for (int i = 1; i <= r[s]; i++) {
-            int s1 = (s + i) % n;
-            int c1 = 1 + seconds(s1, t, r, n);
-            int s2 = (s - i);
-            if (s2 < 0) s2 += n;
-            int c2 = 1 + seconds(s2, t, r, n);
-            int cmin = Math.min(c1, c2);
-            min = Math.min(cmin, min);
+    static class OrderBook {
+        final Map<Integer, Order> allOrders = new HashMap<>();
+        final Map<Side, SortedSet<Order>> limitOrders = new HashMap<>();
+        final SortedSet<Order> stopOrders = new TreeSet<>();
+
+        public OrderBook() {
+            limitOrders.put(Side.buy, new TreeSet<>((o1, o2) -> {
+                int compare = Double.compare(o2.value2, o1.value2);
+                if (compare == 0) compare = o1.compareTo(o2);
+                return compare;
+            }));
+            limitOrders.put(Side.sell, new TreeSet<>((o1, o2) -> {
+                int compare = Double.compare(o1.value2, o2.value2);
+                if (compare == 0) compare = o1.compareTo(o2);
+                return compare;
+            }));
         }
-        return 1 + min;
+
+        SortedSet<Double> execute(Order order) {
+            SortedSet<Order> orders = limitOrders.get(order.side.opposing());
+            SortedSet<Double> executionPrices = new TreeSet<>();
+            while (!orders.isEmpty()) {
+                Order maker = orders.first();
+                if (order.type == OrderType.limit && !order.side.canExecute(order.value2, maker.value2)) break;
+                int volume = Math.min(order.value1, maker.value1);
+                print(order, maker, volume, maker.value2);
+                executionPrices.add(maker.value2);
+                maker.value1 -= volume;
+                if (maker.value1 == 0) {
+                    allOrders.remove(maker.id);
+                    orders.remove(maker);
+                }
+                order.value1 -= volume;
+                if (order.value1 == 0) break;
+            }
+            if (order.type == OrderType.limit && order.value1 > 0) {
+                limitOrders.get(order.side).add(order);
+                allOrders.put(order.id, order);
+            }
+            return executionPrices;
+        }
+
+        void print(Order taker, Order maker, int volume, double price) {
+            System.out.println(String.format("match %s %s %s %.2f", taker.id, maker.id, volume, price));
+        }
+
+        public void process(Order order) {
+            if (order.type == OrderType.cancel) {
+                Order existing = allOrders.get(order.value1);
+                if (existing != null) {
+                    allOrders.remove(existing.id);
+                    if (existing.type == OrderType.limit)
+                        limitOrders.get(existing.side).remove(existing);
+                    else if (existing.type == OrderType.stop)
+                        stopOrders.remove(existing);
+                }
+            } else if (order.type == OrderType.stop) {
+                allOrders.put(order.id, order);
+                stopOrders.add(order);
+            } else {
+                SortedSet<Double> executionPrices = execute(order);
+                if (order.value1 <= 0) {
+                    limitOrders.get(order.side).remove(order);
+                    allOrders.remove(order.id);
+                }
+                if (!executionPrices.isEmpty()) {
+                    for (Iterator<Order> it = stopOrders.iterator(); it.hasNext(); ) {
+                        Order stopOrder = it.next();
+                        if ((stopOrder.side == Side.sell && executionPrices.first() <= stopOrder.value2) || (stopOrder.side == Side.buy && executionPrices.last() >= stopOrder.value2)) {
+                            execute(stopOrder);
+                            allOrders.remove(stopOrder.id);
+                            it.remove();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    enum OrderType {limit, stop, cancel, market}
+
+    enum Side {
+        buy {
+            @Override boolean canExecute(double orderPrice, double price) { return orderPrice >= price; }
+
+            @Override Side opposing() { return sell; }
+        },
+        sell {
+            @Override boolean canExecute(double orderPrice, double price) { return orderPrice <= price; }
+
+            @Override Side opposing() { return buy; }
+        };
+
+        static Side of(String s) {
+            if (s.equals("buy")) return buy;
+            if (s.equals("sell")) return sell;
+            return null;
+        }
+
+        boolean canExecute(double orderPrice, double price) {return false;}
+
+        Side opposing() {return null;}
+    }
+
+    static class Order implements Comparable<Order> {
+        final int id;
+        final OrderType type;
+        final Side side;
+        int value1;
+        double value2;
+
+        Order(int id, String type, String side, int value1, double value2) {
+            this.id = id;
+            this.type = OrderType.valueOf(type);
+            this.side = Side.of(side);
+            this.value1 = value1;
+            this.value2 = value2;
+        }
+
+        @Override public String toString() {
+            final StringBuilder sb = new StringBuilder();
+            sb.append("Order");
+            sb.append("{id=").append(id);
+            sb.append(", type=").append(type);
+            sb.append(", side=").append(side);
+            sb.append(", value1=").append(value1);
+            sb.append(", value2=").append(value2);
+            sb.append('}');
+            return sb.toString();
+        }
+
+        @Override public int compareTo(Order o) { return Integer.compare(id, o.id); }
     }
 }
--- hr/src/bst/Main.java
+++ hr/src/bst/Main.java
@@ -2,21 +2,34 @@ package bst;
 import java.util.*;
 
 public class Main {
-    static Node NULL = new Node();
-    static {
-        NULL.right = NULL;
-        NULL.left = NULL;
+    public static void main(String[] args) {
+        Node root = new Node(6);
+        for (int i = 1; i <= 9; i++) {
+            insert(root, i);
+            insert(root, 9 - i);
+            insert(root, 9);
+            insert(root, 2);
+            insert(root, 3);
+        }
+        print(root);
     }
 
-    public static void main(String[] args) {
-        Node zero = new Node(0, NULL, NULL);
-        Node one = new Node(1, NULL, zero);
-        Node two = new Node(2, NULL, NULL);
-        Node three = new Node(3, NULL, NULL);
-        Node four = new Node(4, one, two);
-        Node six = new Node(6, three, NULL);
-        Node five = new Node(50, four, six);
-        print(five);
+    static void insert(Node root, int i) {
+        Node newNode = new Node(i);
+        Queue<Node> nodes = new ArrayDeque<>();
+        nodes.add(root);
+        while (!nodes.isEmpty()) {
+            Node node = nodes.poll();
+            if (node.left == null) {
+                node.left = newNode;
+                return;
+            } else if (node.right == null) {
+                node.right = newNode;
+                return;
+            }
+            nodes.add(node.left);
+            nodes.add(node.right);
+        }
     }
 
     static void print(Node node) {
@@ -27,18 +40,18 @@ public class Main {
         for (char[] row : grid) Arrays.fill(row, ' ');
         List<Node> nodes = new ArrayList<>();
         nodes.add(node);
-        for (int level = 0; level < 4; level++) {
+        for (int level = 0; level <= h; level++) {
             int childCount = (int) Math.pow(2, level);
             int nodeWidth = w / childCount;
             int start = 0;
             List<Node> children = new ArrayList<>();
             for (Node n : nodes) {
-                if (n != NULL) {
+                if (n != null) {
                     printNode(n, grid, start, start + nodeWidth, level);
                 }
                 start += nodeWidth + 1;
-                children.add(n.left);
-                children.add(n.right);
+                children.add(n == null ? null : n.left);
+                children.add(n == null ? null : n.right);
             }
             nodes = children;
         }
@@ -48,9 +61,9 @@ public class Main {
     static void printNode(Node node, char[][] grid, int s, int e, int level) {
         int middle = (e + s) / 2;
         int q = (e - s) / 4;
-        if (node.left != NULL)
+        if (node.left != null)
             Arrays.fill(grid[level], middle - q, middle, '_');
-        if (node.right != NULL)
+        if (node.right != null)
             Arrays.fill(grid[level], middle + 1, middle + q + 1, '_');
         String d = Integer.toString(node.d);
         for (int i = 0; i < d.length(); i++)
@@ -58,15 +71,14 @@ public class Main {
     }
 
     static void print(char[][] grid) {
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++)
-                System.out.print(grid[i][j]);
+        for (char[] row : grid) {
+            for (char e : row) System.out.print(e);
             System.out.println();
         }
     }
 
     static int height(Node node) {
-        if (node == NULL) return -1;
+        if (node == null) return -1;
         return 1 + Math.max(height(node.left), height(node.right));
     }
 
@@ -74,13 +86,7 @@ public class Main {
         int d;
         Node left, right;
 
-        public Node() { }
-
-        Node(int d, Node left, Node right) {
-            this.d = d;
-            this.left = left;
-            this.right = right;
-        }
+        Node(int d) { this.d = d; }
 
         @Override public String toString() { return Integer.toString(d); }
     }
