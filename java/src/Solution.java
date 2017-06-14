90ccb9f10907e472ce8719094d419ee379526e8a
IgorK <ikuplevich@fitbit.com>
2017-06-14 11:44:04 -0700

    updates

--- java/src/Solution.java
+++ java/src/Solution.java
@@ -1,39 +1,17 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Solution {
-    public List<String> findMissingRanges(int[] nums, int lower, int upper) {
-        List<String> missing = new ArrayList<>();
-        long lastOut = lower;
-        for (int n : nums) {
-            addMissing(lastOut, n - 1L, missing);
-            lastOut = n + 1;
-        }
-        addMissing(lastOut, upper, missing);
-        return missing;
+    public static void main(String[] args) {
+        System.out.println(new Solution().wordsTyping(new String[]{"me", "me"}, 2, 2));
     }
 
-    void addMissing(long lastOut, long nextOut, List<String> ranges) {
-        if (lastOut == nextOut) {
-            ranges.add(Long.toString(lastOut));
-        } else if (lastOut < nextOut)
-            ranges.add(lastOut + "->" + nextOut);
-    }
-
-    public List<String> summaryRanges(int[] nums) {
-        List<String> ranges = new ArrayList<>();
-        if (nums.length == 0) return ranges;
-        int s = nums[0];
-        for (int i = 1; i < nums.length; i++)
-            if (nums[i - 1] + 1 != nums[i]) {
-                ranges.add(range(s, nums[i - 1]));
-                s = nums[i];
+    public int wordsTyping(String[] sentence, int rows, int cols) {
+        int wc = 0;
+        for (int r = 0; r < rows; r++) {
+            int start = wc % sentence.length;
+            for (int i = start, len = 0; len < cols; i = (i + 1) % sentence.length, wc++) {
+                if (len + sentence[i].length() > cols) break;
+                len += sentence[i].length() + 1;
             }
-        ranges.add(range(s, nums[nums.length - 1]));
-        return ranges;
-    }
-
-    String range(int s, int e) {
-        return s == e ? Integer.toString(s) : s + "->" + e;
+        }
+        return wc / sentence.length;
     }
 }
