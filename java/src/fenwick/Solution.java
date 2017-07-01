cb6158b5c8464bdb9697e15d931313118ad66bb0
IgorK <ikuplevich@fitbit.com>
2017-07-01 12:00:08 -0700

    updates

--- java/src/fenwick/Solution.java
+++ java/src/fenwick/Solution.java
@@ -1,10 +1,10 @@
 package fenwick;
 public class Solution {
     public static void main(String[] args) {
-        NumArray a = new NumArray(new int[]{1, 3, 5});
-        System.out.println(a.sumRange(0, 2));
-        a.update(1, 2);
-        System.out.println(a.sumRange(0, 2));
+        int[][] a = {{1, 2}, {3, 4}};
+        NumMatrix m = new NumMatrix(a);
+        System.out.println(m.sumRegion(1, 0, 1, 0));
+
     }
 
     static class NumArray {
@@ -22,7 +22,7 @@ public class Solution {
             int sum = 0;
             for (int j = i + 1; j > 0; j -= j & (-j))
                 sum += bit[j];
-            return sum;         
+            return sum;
         }
 
         void init(int i, int d) {
@@ -38,6 +38,44 @@ public class Solution {
 
         public int sumRange(int i, int j) {
             return sum(j) - sum(i - 1);
+        }
+    }
+
+    static class NumMatrix {
+        int[][] tree, nums;
+        int m, n;
+
+        public NumMatrix(int[][] matrix) {
+            if (matrix.length == 0 || matrix[0].length == 0) return;
+            m = matrix.length;
+            n = matrix[0].length;
+            tree = new int[m + 1][n + 1];
+            nums = new int[m][n];
+            for (int i = 0; i < m; i++)
+                for (int j = 0; j < n; j++)
+                    update(i, j, matrix[i][j]);
+        }
+
+        public void update(int row, int col, int val) {
+            if (m == 0 || n == 0) return;
+            int delta = val - nums[row][col];
+            nums[row][col] = val;
+            for (int i = row + 1; i <= m; i += i & (-i))
+                for (int j = col + 1; j <= n; j += j & (-j))
+                    tree[i][j] += delta;
+        }
+
+        public int sumRegion(int row1, int col1, int row2, int col2) {
+            if (m == 0 || n == 0) return 0;
+            return sum(row2 + 1, col2 + 1) + sum(row1, col1) - sum(row1, col2 + 1) - sum(row2 + 1, col1);
+        }
+
+        public int sum(int row, int col) {
+            int sum = 0;
+            for (int i = row; i > 0; i -= i & (-i))
+                for (int j = col; j > 0; j -= j & (-j))
+                    sum += tree[i][j];
+            return sum;
         }
     }
 }
--- java/src/tree/Solution.java
+++ java/src/tree/Solution.java
@@ -1,4 +1,11 @@
 package tree;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+
 public class Solution {
     int max;
 
@@ -17,6 +24,36 @@ public class Solution {
             count = 1;
         lc(node.left, node.val + 1, count);
         lc(node.right, node.val + 1, count);
+    }
+
+    public List<List<Integer>> verticalOrder(TreeNode root) {
+        List<List<Integer>> r = new ArrayList<>();
+        if (root == null) return r;
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        Queue<TreeNode> nodes = new ArrayDeque<>();
+        Queue<Integer> levels = new ArrayDeque<>();
+        nodes.add(root);
+        levels.add(0);
+        int min = 0, max = 0;
+        while (!nodes.isEmpty()) {
+            TreeNode node = nodes.remove();
+            Integer level = levels.remove();
+            map.putIfAbsent(level, new ArrayList<>());
+            map.get(level).add(node.val);
+            if (node.left != null) {
+                nodes.add(node.left);
+                levels.add(level - 1);
+                min = Math.min(level - 1, min);
+            }
+            if (node.right != null) {
+                nodes.add(node.right);
+                levels.add(level + 1);
+                max = Math.max(level + 1, max);
+            }
+        }
+        for (int i = min; i <= max; i++)
+            r.add(map.get(i));
+        return r;
     }
 
     public class TreeNode {
