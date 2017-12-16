394fcacb5516cf21a21c4ea4a5407e90973a4533
IgorK <ikuplevich@fitbit.com>
2017-12-16 10:37:43 -0500

    updates

---
+++ java/src/daily_temperatures/Solution.java
@@ -0,0 +1,17 @@
+package daily_temperatures;
+import java.util.Stack;
+
+public class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stack = new Stack<>();
+        int[] ret = new int[temperatures.length];
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int idx = stack.pop();
+                ret[idx] = i - idx;
+            }
+            stack.push(i);
+        }
+        return ret;
+    }
+}
