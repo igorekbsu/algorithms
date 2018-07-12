dd630ebd5905d308792916e3195a931600868b00
IgorK <ikuplevich@fitbit.com>
2018-07-12 13:53:19 -0400

    updates

--- java/src/next_closest_time/Solution.java
+++ java/src/next_closest_time/Solution.java
@@ -1,21 +1,16 @@
 package next_closest_time;
 
 public class Solution {
-    public static void main(String[] args) {
-        System.out.println(new Solution().nextClosestTime("23:59"));
+    public String nextClosestTime(String time) {
+        char[] a = time.toCharArray();
+        int h = 10 * (a[0] - '0') + a[1] - '0', m = 10 * (a[3] - '0') + a[4] - '0';
+        for (int used = mask(h, m), curr = -1; (used & curr) != curr; curr = mask(h, m)) {
+            m = m == 59 ? 0 : m + 1;
+            if (m == 0)
+                h = h == 23 ? 0 : h + 1;
+        }
+        return String.format("%02d:%02d", h, m);
     }
 
-    public String nextClosestTime(String t) {
-        int h = 10 * (t.charAt(0) - '0') + t.charAt(1) - '0';
-        int m = 10 * (t.charAt(3) - '0') + t.charAt(4) - '0';
-        int used = (1 << h / 10) | (1 << h % 10) | (1 << m / 10) | (1 << m % 10);
-        while (true) {
-            m = m + 1 == 60 ? 0 : m + 1;
-            if (m == 0)
-                h = h + 1 == 24 ? 0 : h + 1;
-            int current = (1 << h / 10) | (1 << h % 10) | (1 << m / 10) | (1 << m % 10);
-            if ((used & current) == current)
-                return String.format("%02d:%02d", h, m);
-        }
-    }
+    int mask(int h, int m) { return (1 << h / 10 | 1 << h % 10 | 1 << m / 10 | 1 << m % 10);}
 }
